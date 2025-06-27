6e103e0ffccd650960c228982421f354b42a04d2
Andrea Di Nenno <dinennoandrea@gmail.com>
2025-06-27 20:17:18 +0200

    test

--- frontend/app/src/screens/DashboardScreen/DashboardScreen.tsx
+++ frontend/app/src/screens/DashboardScreen/DashboardScreen.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = 'force-dynamic';
 
 import { getBranches, useLiquityStats } from "@/src/liquity-utils";
 import { TransparencyMetrics } from "./TransparencyMetrics";
