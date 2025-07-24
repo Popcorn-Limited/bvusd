06c8739aa39a891ca19c92a42d31f1ca809a6bce
Andrea <dinennoandrea@gmail.com>
2025-07-24 12:30:30 +0200

    Increase number of swaps

--- frontend/stats-script/src/constants.ts
+++ frontend/stats-script/src/constants.ts
@@ -7,4 +7,4 @@ export const SP_DEPOSITS_QUERY = "https:/api.dune.com/api/v1/query/5156890/resul
 export const RESERVES_QUERY = "https:/api.dune.com/api/v1/query/5492104/results?limit=1000";
 export const bvUSD_HOLDERS = "https:/api.dune.com/api/v1/query/5517514/results?limit=1000";
 export const SUSHI_VOLUME = "https:/api.dune.com/api/v1/query/5523901/results?limit=1000";
-export const SUSHI_SWAPS = "https:/api.dune.com/api/v1/query/5529727/results?limit=10";
+export const SUSHI_SWAPS = "https:/api.dune.com/api/v1/query/5529727/results?limit=1000";
