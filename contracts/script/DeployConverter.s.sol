50e0e2e9ceed44786d12e4737ed1c2d5a4ea907a
Andrea <dinennoandrea@gmail.com>
2025-10-13 14:20:21 +0200

    Add deploy whitelist

--- contracts/script/DeployConverter.s.sol
+++ contracts/script/DeployConverter.s.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.24;
 
 import {Script, console} from "forge-std/Script.sol";
 import {BoldConverter, Path, IWhitelist, IBoldToken, IERC20Metadata} from "src/Dependencies/BoldConverter.sol";
+import {Whitelist} from "src/Dependencies/Whitelist.sol";
 
 contract DeployConverter is Script {
     function run() public returns (BoldConverter converter) {
@@ -15,15 +16,15 @@ contract DeployConverter is Script {
 
         // Edit this
         underlyings[0] = IERC20Metadata(
-            0x203A662b0BD271A6ed5a60EdFbd04bFce608FD36
+            0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
         ); // usdc
         paths[0] = Path(
             address(0x452DC676b4E377a76B4b3048eB3b511A0F1ec057),
             6,
             0
         );
-        address bvUSD = 0x876aac7648D79f87245E73316eB2D100e75F3Df1;
-        address whitelist = 0x83BBAA022Cca1295a975EC101a073C44Ea336f79;
+        address bvUSD = 0x9BC2F611fa2196E097496B722f1CBCDfE2303855;
+        address whitelist = 0x788DbB1888a50e97837b9D06Fd70db107b082A12;
         address owner = 0x452DC676b4E377a76B4b3048eB3b511A0F1ec057;
 
         converter = new BoldConverter(
@@ -33,6 +34,18 @@ contract DeployConverter is Script {
         );
         converter.setWhitelist(IWhitelist(whitelist));
         converter.nominateNewOwner(owner);
+
+        vm.stopBroadcast();
+    }
+}
+
+contract DeployWhitelist is Script {
+     function run() public returns (Whitelist whitelist) {
+        vm.startBroadcast();
+        console.log("msg.sender:", msg.sender);
+        address owner = address(0);
+
+        whitelist = new Whitelist(owner);
 
         vm.stopBroadcast();
     }
